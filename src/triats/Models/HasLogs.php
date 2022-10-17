6dd32a53651ef5e8b91d1d1c1761f16282a86b6f
ntut-mika <t107590003@ntut.org.tw>
2022-10-17 12:49:14 +0800

    refactor

--- src/triats/Models/HasLogs.php
+++ src/triats/Models/HasLogs.php
@@ -9,15 +9,15 @@ use Mika\Logger\Models\Log;
 
 trait HasLogs
 {
-    public function logs(ModelActionEnum $type = null)
+    public function logs(ModelActionEnum $action = null)
     {
         $query = $this->hasMany(Log::class, 'content->' . $this->getKeyName())
                     ->where('type', LogTypeEnum::Model)
                     ->where('content->class', get_class($this))
                     ->orderBy('id', 'desc');
 
-        if ($type !== null) {
-            $query->where('content->action', $type);
+        if ($action !== null) {
+            $query->where('content->action', $action);
         }
 
         return $query;
